c211a3977f6367bfa38bc288ad5f5a86f2546399
Samurnihs <43216249+Samurnihs@users.noreply.github.com>
2019-04-21 22:13:43 +0300

    First commit.

---
+++ multiplier.py
@@ -0,0 +1,37 @@
+import sys
+import cv2 as cv
+import numpy as np
+import os
+
+def data_names(way): # функция, возвращающая массив с именами всех файлов в папке
+    files = list() # массив с полными путями к файлам
+    for (dirpath, dirnames, filenames) in os.walk(way): # считываем все имена файлов в папке и добавляем их в массив
+        files.extend(filenames)
+        break 
+    return files
+
+
+def mul_im(im_path1, im_path2): # функция, перемножающая изображения попиксельно
+    img1 = cv.cvtColor(cv.imread(im_path1), cv.COLOR_BGR2GRAY) # чтение изображения 1
+    img2 = cv.cvtColor(cv.imread(im_path2), cv.COLOR_BGR2GRAY) # чтение изображения 2
+    out_mask = (img1 / 255) * (img2 / 255) * 255 # перемножаем изображения
+    return out_mask
+
+
+input_fold1 = 'greenfold/' # папка, из которой берём зелёные картинки
+input_fold2 = 'redfold/' # папка, из которой берём красные картинки
+output_fold = 'result/' # папка, в которую сохраняем продукт
+
+FileNamesGreen = data_names(input_fold1) # список имён зелёных файлов
+FileNamesRed = data_names(input_fold2) # список имён красных файлов
+InputFileWayGreen = list() # полные пути ко входным зелёным файлам
+InputFileWayRed = list() # полные пути ко входным красным файлам
+ResultFileWay = list() # полные пути к выходным файлам
+for f in FileNamesGreen:
+    InputFileWayGreen.append(input_fold1 + f)  # пути ко входным зелёным файлам
+    ResultFileWay.append(output_fold + f) # пути к выходным файлам
+for f in FileNamesRed:
+    InputFileWayRed.append(input_fold2 + f) # пути ко входным красным файлам
+
+for i in range(len(InputFileWayGreen)):
+    cv.imwrite(ResultFileWay[0], mul_im(InputFileWayGreen[i], InputFileWayRed[i]))
